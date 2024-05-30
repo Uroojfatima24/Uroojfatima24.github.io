8192c4d384951949d9b468da6f9aad7bf4576316
Uroojfatima24 <144207316+Uroojfatima24@users.noreply.github.com>
2024-05-30 22:55:52 +0400

    locations.js

--- locations.js
+++ locations.js
@@ -1,4 +1,9 @@
-function toggleBubble(bubbleId) {
+/* Defines a function named toggleBubble that takes the ID of the bubble element as a parameter. */
+function toggleBubble(bubbleId) { 
+/* Retrieves the bubble element from the document based on its ID. */
     var bubble = document.getElementById(bubbleId);
+/* Toggles the presence of the CSS class show on the bubble element. If the class is present, 
+it is removed; if it is absent, it is added. This class likely controls the visibility of the bubble, 
+as defined in the CSS .bubble-container.show rule. */
     bubble.classList.toggle('show');
 }
